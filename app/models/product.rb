68d5e6e9f1e892c8b27b511bddb71afb9e4e6ba4
Ruslan Rozhkov <bladteam@gmail.com>
2016-11-15 13:04:53 +0200

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,19 @@ class Product < ActiveRecord::Base
     Product.order(:updated_at).last
   end
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+  end
 end
